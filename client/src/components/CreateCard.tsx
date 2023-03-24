67b5b663e048f87adf98925dc97067ca1f8f0a31
Artyom_G_ <ginoyan.artyom@gmail.com>
2023-03-25 02:53:21 +0400

    add createcard component

---
+++ client/src/components/CreateCard.tsx
@@ -0,0 +1,68 @@
+import { FC, useRef, useState } from "react";
+import Input from "./Input";
+import Buttons from "./Buttons";
+import { useOutsideClick } from "../hooks/useOutsideClick";
+
+import columnStyles from "../assets/css/column.module.css";
+import { useSelector } from "react-redux";
+import { userSelector } from "../redux/auth/authSelector";
+import { useDispatch } from "react-redux";
+import { addCardAction } from "../redux/board/boardSlice";
+
+export interface IAppProps {
+  id: number;
+  cardsLlength: number;
+}
+
+const CreateCard: FC<IAppProps> = (props) => {
+  const dispatch = useDispatch();
+  const wrapperRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const user = useSelector(userSelector);
+  const [visible, setVisible] = useState(false);
+  useOutsideClick(wrapperRef, setVisible);
+
+  const createNewCard = (e: any) => {
+    dispatch(
+      addCardAction({
+        userId: user.id,
+        content: inputRef.current?.value,
+        columnId: +e.target.id,
+        position: props.cardsLlength,
+      }),
+    );
+  };
+
+  return (
+    <div className={columnStyles.createContainer} ref={wrapperRef}>
+      {!visible && (
+        <>
+          <div
+            onClick={() => {
+              setVisible(true);
+            }}
+            className={columnStyles.createWrap}>
+            <div>+</div>
+            <h3>Create Card</h3>
+          </div>
+        </>
+      )}
+      {visible && (
+        <>
+          <Input
+            autoFocus={true}
+            inputRef={inputRef}
+            className={columnStyles.headerInput}
+            classContainer={columnStyles.headerInputWrap}
+          />
+          <Buttons
+            setVisible={setVisible}
+            getValue={createNewCard}
+            id={props.id}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+export default CreateCard;
