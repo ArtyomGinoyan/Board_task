73652b0cbedd188c24243a0bc85bb6f53b70942b
Artyom_G_ <ginoyan.artyom@gmail.com>
2023-03-26 00:19:43 +0400

    add remove card

---
+++ client/src/services/removedCardService.ts
@@ -0,0 +1,14 @@
+import { RemoveCard } from "../types/cardTypes";
+
+const removeCardService = async (form: RemoveCard): Promise<Response> => {
+  const response = await fetch(`http://localhost:3303/card/remove/${form.id}/${form.position}/${form.columnId}`, {
+    method: "DELETE",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return response;
+};
+
+export default removeCardService;
