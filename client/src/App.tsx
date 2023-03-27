a40c4b74ecf253d76bf8dd6bcf6b1bcf784cf250
Artyom_G_ <ginoyan.artyom@gmail.com>
2023-03-27 13:24:01 +0400

    modified  files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,38 @@
-import { Route, Routes } from "react-router-dom";
-import Board from "./pages/board/Board";
-import NotFound from "./pages/notfound/NotFound";
-import AuthLayout from "./layouts/AuthLayout";
-import PublicLayout from "./layouts/PublicLayout";
-import Login from "./pages/autorization/Login";
-import Register from "./pages/autorization/Register";
+import { Route, Routes } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import { ToastContainer } from 'react-toastify';
 
-import appStyles from "./app.module.css";
+import Board from './pages/board/Board';
+import NotFound from './pages/notfound/NotFound';
+import AuthLayout from './layouts/AuthLayout';
+import PublicLayout from './layouts/PublicLayout';
+import Login from './pages/autorization/Login';
+import Register from './pages/autorization/Register';
+
+import appStyles from './assets/css/app.module.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  return (
-    <div className={appStyles.App}>
-      <Routes>
-        <Route element={<PublicLayout />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Route>
-        <Route element={<AuthLayout />}>
-          <Route path="/" element={<Board />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
-  );
+	return (
+		<>
+			<ToastContainer />
+			<AnimatePresence>
+				<div className={appStyles.App}>
+					<Routes>
+						<Route element={<PublicLayout />}>
+							<Route path="/login" element={<Login />} />
+							<Route path="/register" element={<Register />} />
+						</Route>
+						<Route element={<AuthLayout />}>
+							<Route path="/" element={<Board />} />
+							<Route path="/board" element={<Board />} />
+						</Route>
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</div>
+			</AnimatePresence>
+		</>
+	);
 };
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { store } from "./redux/store";
-import { Provider } from "react-redux";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from "./App";
-import "./index.css";
+import App from './App';
+import './assets/css/index.css';
 
-const container = document.getElementById("root")!;
+const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>
 );
